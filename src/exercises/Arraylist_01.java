2deb1936f753cc56a7280abb574ae3f7c251998e
thkhaled99 <thairkhaled909@gmail.com>
2024-05-28 22:56:14 -0400

    Ticket_6554

--- src/exercises/Arraylist_01.java
+++ src/exercises/Arraylist_01.java
@@ -1,17 +1,23 @@
 package exercises;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Arraylist_01 {
 	
 	
-	public void printArrList () {
+	public static void printArrList() {
 	List<String> options = new ArrayList<String>();
 	options.add("jack");
 	options.add("optisom");
 	options.add("hackar");
 	
+	Iterator<String> ittrIterator = options.iterator();
+	
+	while (ittrIterator.hasNext()) {
+		System.out.println(ittrIterator.next().toString());
+	}
 	}
 	
 }
