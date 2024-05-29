991da0d8e9acbce26192406f8f0d1d4953cd81a0
thkhaled99 <thairkhaled909@gmail.com>
2024-05-28 22:45:59 -0400

    Ticket_6554

---
+++ src/exercises/Arraylist_01.java
@@ -0,0 +1,17 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist_01 {
+	
+	
+	public void printArrList () {
+	List<String> options = new ArrayList<String>();
+	options.add("jack");
+	options.add("optisom");
+	options.add("hackar");
+	
+	}
+	
+}
